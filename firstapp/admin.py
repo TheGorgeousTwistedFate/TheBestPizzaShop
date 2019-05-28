45d1d84770223997b54207e66fc3ea37d9036287
twistedfate <artur.vinniklol@gmail.com>
2019-05-28 16:31:44 +0300

    Cоздал панель для создания пицерий в панели админа

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from TheBestPizzaShop.models import PizzaShop
+# Register your models here.
 
-# Register your models here./
+admin.site.registr(PizzaShop)
--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class PizzaShop(models.Model):
+    name = models.CharField(max_length= 30, verbose_name ='Название')
+    description = models.TextField(verbose_name='Описание')
+    rating = models.FloatField(default = 0 ,verbose_name ='Рейтинг')
+    url = models.URLField(verbose_name='Интернет сылка на пицерию')
+
+    def __str__(self):
+        return self.name
+
